306f489829b5b3770c323019729bd93df03891e1
Roman <admin@Romans-MacBook-Pro.local>
2026-03-05 14:40:35 +0100

    feat(Docker): add  docker-compose 		[+] add nginx settings 		[+] add php for docker with alpine

--- public/index.php
+++ public/index.php
@@ -1,7 +1,15 @@
-<?php
+<?php declare(strict_types=1);
 
 require __DIR__ . "/../vendor/autoload.php";
 
 use App\Test;
 
-echo Test::hello();
+$test = new Test();
+
+header("Content-Type: application/json");
+echo json_encode([
+	"message" => "Docker is working",
+	"test_class" => get_class($test),
+	"uri" => $_SERVER["REQUEST_URI"],
+	"method" => $_SERVER["REQUEST_METHOD"],
+]);
